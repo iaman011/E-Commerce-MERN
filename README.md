c9c2f6823f49d4d772f3d8428312ce57894daa74
Aman <iaman.singh011@gmail.com>
2025-03-12 13:41:29 +0530

    changes commit

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ npm install @types/bcrypt --save-dev
 npm install --save-dev @types/jsonwebtoken
 
 npm run dev 
+http://localhost:3001
 
 cd client
 npm i axios react-cookie react-router-dom @fortawesome/react-fontawesome @fortawesome/free-solid-svg-icons @fortawesome/fontawesome-svg-core
+
+
+npm run dev
+http://localhost:5173
--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,11 +1,63 @@
-
+import { useState, SyntheticEvent } from "react";
+import axios from "axios";  //in order to make API request
 
 export const AuthPage = () => {
   return (
-    <div>
-      
+    <div className="auth">
+      <Register />
+      <Login />
     </div>
-  )
-}
+  );
+};
 
-export default AuthPage
+const Register = () => {
+
+  const [username, setUsername ]= useState("");
+  const [password, setPassword ]= useState("");
+
+  // creating a function, whenever you submit the form this function is called
+  const handleSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault();     // Prevents form from reloading the page
+    // in order to register the user, it is a post req
+    await axios.post ("http://localhost:3001/user/register", {
+      username,
+      password
+    });
+
+    alert("Registration Completed! New Login.");
+  }
+  return (
+    <div className="auth-container">
+      <form onSubmit={handleSubmit}>          {/* called the handleSubmit fn over here */}
+        <h2> Register </h2>
+        <div className="form-group">
+          <label htmlFor="username"> Username: </label>
+          <input 
+          type="text" 
+          id="username" 
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password"> Password: </label>
+          <input 
+          type="password" 
+          id="password" 
+          value={password}
+          onChange={(event)=> setPassword(event.target.value)}
+          required />
+        </div>
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default AuthPage;
